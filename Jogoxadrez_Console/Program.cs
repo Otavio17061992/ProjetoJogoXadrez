03db5cb485bb895997badbb79ffe0378cf1eeb96
Joao Otavio <joaoalmeida48@outlook.com>
2023-02-21 21:42:47 -0300

    Criando a class Posicao

--- Jogoxadrez_Console/Program.cs
+++ Jogoxadrez_Console/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Jogoxadrez_Console.Tabuleiro;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Posicao P;
+
+        P = new Posicao(3, 4);
+
+        Console.WriteLine("Posição: " + P);
+
+        Console.ReadLine();
+    }
+}
